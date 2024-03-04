b71ff2493c0933a8e5a98d1b9459f19f1edf9292
yenslife <77geo5rge6@gmail.com>
2024-03-04 15:06:23 +0800

    update shell script

--- data_collect.sh
+++ data_collect.sh
@@ -1,12 +1,12 @@
 #!/bin/bash 
 
 run_mlgame() {
-    python -m mlgame -1 -f 9000 -i ./ml/ml_play_web.py . --difficulty NORMAL --level $1
+    python -m mlgame -1 --nd -f 9000 -i ./ml/ml_play_collect.py . --difficulty NORMAL --level $1
 }
 
 for ((level=1; level<=25; level++))
 do
-    for ((i=1; i<=3; i++))
+    for ((i=1; i<=2; i++))
     do
         run_mlgame $level
     done
